da5f31e10a0e39ddefe11f3c380271a79ebdd656
teun_ <teun_@DESKTOP-81EBD61>
2021-03-14 14:38:16 +0100

    Cleanup

--- Sources/Silvester.Pathfinder.Official.Web/Pages/Races/RacesOverview.razor.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Silvester.Pathfinder.Official.Web.Pages.Overviews.Races
-{
-    public partial class RacesOverview : BaseOverviewPage
-    {
-        protected override string Title => "Races";
-    }
-}
