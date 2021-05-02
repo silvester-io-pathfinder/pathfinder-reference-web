cf9e4cad2b52e4838c3da6ad5b0e9da9d5d0edd2
teun_ <teun_@DESKTOP-81EBD61>
2021-05-02 20:48:35 +0200

    Version 0.0.26 - Added alchemical elixirs.

---
+++ Sources/Silvester.Pathfinder.Official.Web/Components/Tables/Instances/AlchemicalElixirs/Providers/AlchemicalElixirPageProvider.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using Silvester.Pathfinder.Official.Web.Graphql.Generated;
+using StrawberryShake;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Silvester.Pathfinder.Official.Web.Components.Tables.Instances.AlchemicalElixirs.Providers
+{
+    public class AlchemicalElixirPageProvider : GraphqlPageProvider<IAlchemicalElixirsPage, AlchemicalElixirSortInput>
+    {
+        public AlchemicalElixirPageProvider(IPathfinderOfficialApi api)
+            : base(api)
+        {
+
+        }
+
+        public override async Task<TableData<IAlchemicalElixirsPage>> GetPage(AlchemicalElixirSortInput[] sortInputs, TableState state, string searchTerm)
+        {
+            IOperationResult<IGetAlchemicalElixirsPageResult>? result = await PathfinderOfficialApi.GetAlchemicalElixirsPage
+                     .ExecuteAsync(state.Page * state.PageSize, state.PageSize, searchTerm, sortInputs);
+
+            if (result.Data?.AlchemicalElixirs == null)
+            {
+                return EmptyPage();
+            }
+
+            TableData<IAlchemicalElixirsPage> page = new TableData<IAlchemicalElixirsPage>()
+            {
+                TotalItems = result.Data!.AlchemicalElixirs!.TotalCount,
+                Items = result.Data!.AlchemicalElixirs!.Items!
+            };
+
+            return page;
+        }
+    }
+}
