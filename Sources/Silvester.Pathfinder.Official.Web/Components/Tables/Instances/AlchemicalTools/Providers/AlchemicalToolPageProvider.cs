202d68d5f48ee95d6942c3f60f511d0b99d80b45
teun_ <teun_@DESKTOP-81EBD61>
2021-05-02 21:02:12 +0200

    Version 0.0.27 - Added alchemical tools.

---
+++ Sources/Silvester.Pathfinder.Official.Web/Components/Tables/Instances/AlchemicalTools/Providers/AlchemicalToolPageProvider.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using Silvester.Pathfinder.Official.Web.Graphql.Generated;
+using StrawberryShake;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Silvester.Pathfinder.Official.Web.Components.Tables.Instances.AlchemicalTools.Providers
+{
+    public class AlchemicalToolPageProvider : GraphqlPageProvider<IAlchemicalToolsPage, AlchemicalToolSortInput>
+    {
+        public AlchemicalToolPageProvider(IPathfinderOfficialApi api)
+            : base(api)
+        {
+
+        }
+
+        public override async Task<TableData<IAlchemicalToolsPage>> GetPage(AlchemicalToolSortInput[] sortInputs, TableState state, string searchTerm)
+        {
+            IOperationResult<IGetAlchemicalToolsPageResult>? result = await PathfinderOfficialApi.GetAlchemicalToolsPage
+                     .ExecuteAsync(state.Page * state.PageSize, state.PageSize, searchTerm, sortInputs);
+
+            if (result.Data?.AlchemicalTools == null)
+            {
+                return EmptyPage();
+            }
+
+            TableData<IAlchemicalToolsPage> page = new TableData<IAlchemicalToolsPage>()
+            {
+                TotalItems = result.Data!.AlchemicalTools!.TotalCount,
+                Items = result.Data!.AlchemicalTools!.Items!
+            };
+
+            return page;
+        }
+    }
+}
