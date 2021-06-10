e5f0c923e81f64c10d22488324d654a1cbbc38ea
teun_ <teun_@DESKTOP-81EBD61>
2021-06-10 18:10:15 +0200

    Removed /artifacts from .gitignore.

---
+++ Sources/Silvester.Pathfinder.Official.Web/Components/Tables/Instances/Artifacts/Providers/ArtifactPageProvider.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using Silvester.Pathfinder.Official.Web.Graphql.Generated;
+using StrawberryShake;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Silvester.Pathfinder.Official.Web.Components.Tables.Instances.Artifacts.Providers
+{
+    public class ArtifactPageProvider : GraphqlPageProvider<IArtifactsPage, ArtifactSortInput>
+    {
+        public ArtifactPageProvider(IPathfinderOfficialApi api)
+            : base(api)
+        {
+
+        }
+
+        public override async Task<TableData<IArtifactsPage>> GetPage(ArtifactSortInput[] sortInputs, TableState state, string searchTerm)
+        {
+            IOperationResult<IGetArtifactsPageResult> result = await PathfinderOfficialApi.GetArtifactsPage
+                     .ExecuteAsync(state.Page * state.PageSize, state.PageSize, searchTerm, sortInputs);
+
+            if (result?.Data?.Artifacts == null)
+            {
+                return EmptyPage();
+            }
+
+            TableData<IArtifactsPage> page = new TableData<IArtifactsPage>()
+            {
+                TotalItems = result.Data!.Artifacts!.TotalCount,
+                Items = result.Data!.Artifacts!.Items!
+            };
+
+            return page;
+        }
+    }
+}
