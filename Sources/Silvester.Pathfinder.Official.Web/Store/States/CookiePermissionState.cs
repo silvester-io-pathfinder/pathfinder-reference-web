75ac60e6ffe198903d8e387d6b9d84520ed3f5f0
teun_ <teun_@DESKTOP-81EBD61>
2021-10-04 13:23:42 +0200

    Version 0.0.45 - Added cookie permission banner.

---
+++ Sources/Silvester.Pathfinder.Official.Web/Store/States/CookiePermissionState.cs
@@ -0,0 +1,41 @@
+﻿using Fluxor;
+using Silvester.Pathfinder.Official.Web.Store.Middlewares.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Silvester.Pathfinder.Official.Web.Store.States
+{
+    public record CookiePermissionState
+    {
+        public bool HasAccepted { get; init; }
+    }
+
+    public record SetCookiePermissionAcceptedAction : IPersistedAction
+    {
+        public bool HasAccepted { get; }
+
+        public string Identifier => "CookiePermissionsAccepted";
+
+        public SetCookiePermissionAcceptedAction(bool hasAccepted)
+        {
+            HasAccepted = hasAccepted;
+        }
+    }
+
+    public class CookiePermissionFeature : Feature<CookiePermissionState>
+    {
+        public override string GetName() => "CookiePermission";
+        protected override CookiePermissionState GetInitialState() => new CookiePermissionState();
+
+        [ReducerMethod]
+        public static CookiePermissionState SetAccepted(CookiePermissionState state, SetCookiePermissionAcceptedAction action)
+        {
+            return state with
+            {
+                HasAccepted = action.HasAccepted
+            };
+        }
+    }
+}
