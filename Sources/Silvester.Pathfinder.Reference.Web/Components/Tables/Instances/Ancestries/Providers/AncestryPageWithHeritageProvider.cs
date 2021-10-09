8604c4e64cf82d9f2d0c58484463d478018838fe
Teun Kooijman <teun_@DESKTOP-81EBD61>
2021-10-09 12:14:20 +0200

    Version 0.2.6 - Added Heritage details and overview pages.

---
+++ Sources/Silvester.Pathfinder.Reference.Web/Components/Tables/Instances/Ancestries/Providers/AncestryPageWithHeritageProvider.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+using Silvester.Pathfinder.Reference.Web.Graphql.Generated;
+using StrawberryShake;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Silvester.Pathfinder.Reference.Web.Components.Tables.Instances.Ancestries.Providers
+{
+    public class AncestryPageWithHeritageProvider : GraphqlPageProvider<IAncestriesPage, AncestrySortInput>
+    {
+        private Guid HeritageId { get; }
+
+        public AncestryPageWithHeritageProvider(IPathfinderReferenceApi api, Guid heritageId)
+            : base(api)
+        {
+            HeritageId = heritageId;
+        }
+
+        public override async Task<TableData<IAncestriesPage>> GetPage(AncestrySortInput[] sortInputs, TableState state, string searchTerm)
+        {
+            IOperationResult<IGetAncestriesPageWithHeritageResult> result = await PathfinderReferenceApi.GetAncestriesPageWithHeritage
+                     .ExecuteAsync(state.Page * state.PageSize, state.PageSize, HeritageId, searchTerm, sortInputs);
+
+            if (result?.Data?.Ancestries == null)
+            {
+                return EmptyPage();
+            }
+
+            TableData<IAncestriesPage> page = new TableData<IAncestriesPage>()
+            {
+                TotalItems = result.Data!.Ancestries!.TotalCount,
+                Items = result.Data!.Ancestries!.Items!
+            };
+
+            return page;
+        }
+    }
+}
